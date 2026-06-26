0b6e2ca826a263597b3d23d3a8c367e18324eeee
CahyoAditya <adityacahyo104@gmail.com>
2026-06-26 20:14:36 +0700

    feat: add ghost-girl model and implement face-to-face spawn event at 80% threshold

--- Scripts/Systems/EventConfig.gd
+++ Scripts/Systems/EventConfig.gd
@@ -2,6 +2,6 @@ class_name EventConfig
 extends Resource
 
 @export var deadline_seconds := 1200.0
-@export var thresholds: Array[int] = [25, 50, 75, 99, 100]
+@export var thresholds: Array[int] = [25, 50, 75, 80, 99, 100]
 @export var jumpscare_duration := 1.5
 @export var kernel_panic_auto_reboot_time := 10.0
